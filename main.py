e898a9613701c0567c1ea99f71caff5b90129e5e
thorrow <99120820+thorrororo@users.noreply.github.com>
2023-07-06 00:01:28 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from requests import get
-print("KURSY WALUT")
-a = input("Podaj walutę (USD/JPY/EUR/CHF/GBP): ")
-page = get(f"http://api.nbp.pl/api/exchangerates/rates/a/{a}/today/?format=json")
-d = page.json()
-k = d["rates"][0]["mid"]
-print(f"1 {a} = {k} PLN")
